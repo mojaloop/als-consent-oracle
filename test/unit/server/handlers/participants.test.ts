afd87320af89687a55bf5ee7896cd1b874bc285a
Kevin Leyow <kleyow@gmail.com>
2021-01-22 09:25:14 -0500

    chore: fix missed test

--- test/unit/server/handlers/participants.test.ts
+++ test/unit/server/handlers/participants.test.ts
@@ -1,4 +1,4 @@
-import { Request } from '@hapi/hapi'
+import { Request, ResponseToolkit } from '@hapi/hapi'
 import * as Handler from '~/server/handlers/participants'
 import Boom from '@hapi/boom'
 import {
@@ -11,7 +11,7 @@ jest.mock('~/shared/logger')
 describe('server/handler/participants', (): void => {
   describe('POST Handler', (): void => {
     it('should return a not implemented error.', async (): Promise<void> => {
-      const req = postParticipantsRequest as Request
+      const req = postParticipantsRequest as unknown as Request
       const response = await Handler.post(
         {
           method: req.method,
@@ -21,7 +21,7 @@ describe('server/handler/participants', (): void => {
           headers: req.headers
         },
         req,
-        h)
+        h as unknown as ResponseToolkit)
       expect(response).toStrictEqual(Boom.notImplemented())
     })
   })
