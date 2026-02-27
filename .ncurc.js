e69c0529822fe558b51788c229537dd2fe9d160f
Juan Correa <5475265+gibaros@users.noreply.github.com>
2026-02-27 12:25:09 -0500

    chore(ci): update CircleCI orb to 1.1.16 (#62)

--- .ncurc.js
+++ .ncurc.js
@@ -3,5 +3,7 @@ module.exports = {
     // Upgrading sqlite past 5.0.2 seems to introduce sh: 1: node-pre-gyp: not found.
     // Investigation needed. Updated to 5.1.7 in the meantime with no issues.
     // 'sqlite3',
+    // eslint v10.x breaks eslint-plugin-import compatibility
+    'eslint',
   ]
 }
